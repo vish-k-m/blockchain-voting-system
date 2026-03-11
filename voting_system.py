93f65efa1bda73cc7f72880b8849f3774c1336df
vish-k-m <muthinirose031@gmail.com>
2026-03-11 20:17:02 +0300

    Update voting_system.py

--- voting_system.py
+++ voting_system.py
@@ -1,1 +1,19 @@
+from blockchain import Blockchain
 
+blockchain = Blockchain()
+
+candidates = ["Alice", "Bob", "Charlie"]
+voted_users = []
+
+def cast_vote(voter, candidate):
+
+    if voter in voted_users:
+        return "You already voted!"
+
+    if candidate not in candidates:
+        return "Invalid candidate."
+
+    blockchain.add_vote(voter, candidate)
+    voted_users.append(voter)
+
+    return "Vote successfully recorded!"
